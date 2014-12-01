bfa972cf62bb2114515707d463c8ffd6b6f03a8f
James Fitzpatrick <thefishlive@hotmail.com>
2014-12-01 20:58:24 +0000

    Allow login by name or id

--- scripts/v1/endpoints/HandshakeEndpoint.php
+++ scripts/v1/endpoints/HandshakeEndpoint.php
@@ -7,11 +7,22 @@ class HandshakeEndpoint extends Endpoint
     {
         $data = json_decode($body);
 
-        if (!isset($data->{"user-id"}) || !isset($data->{"client-id"})) {
+        if (!(isset($data->{"user-id"}) || isset($data->{"display-name"})) || !isset($data->{"client-id"})) {
             throw new EndpointExecutionException("Invalid request");
         }
 
-        $userid = Token::decode($data->{"user-id"});
+        if (isset($data->{"user-id"})) {
+            $userid = Token::decode($data->{"user-id"});
+            $query = "SELECT `name` FROM " . DATABASE_TABLE_USERS . " WHERE `id`='" . $userid->toString() . "';";
+            $result = Database::query($query);
+            $displayname = Database::fetch_data($result)["name"];
+        } elseif (isset($data->{"display-name"})) {
+            $displayname = $data->{"display-name"};
+            $query = "SELECT `id` FROM " . DATABASE_TABLE_USERS . " WHERE `name`='" . $displayname . "';";
+            $result = Database::query($query);
+            $userid = Token::decode(Database::fetch_data($result)["id"]);
+        }
+
         $clientid = Token::decode($data->{"client-id"});
         $token = Token::generateToken(TOKEN_REQUEST, $userid->getUserSecret());
 
@@ -23,7 +34,7 @@ class HandshakeEndpoint extends Endpoint
         }
 
         return array("client-id" => $data->{"client-id"},
-            "user-id" => $userid->toString(),
+            "user" => array ("user-id" => $userid->toString(), "display-name" => $displayname),
             "request-token" => array("token" => $token->toString(), "expires" => 3600));
     }
 }
